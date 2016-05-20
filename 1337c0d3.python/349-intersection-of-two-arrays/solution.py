c8cdc1c61993be182cc5dce9c6b10976cc249659
Chen Gui <gui.g.chen@gmail.com>
2016-05-20 11:16:18 +0800

    Submit 'Intersection of Two Arrays' solution
    60 / 60 test cases passed.
    Status: Accepted
    Runtime: 56 ms
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/349-intersection-of-two-arrays/solution.py
+++ 1337c0d3.python/349-intersection-of-two-arrays/solution.py
@@ -10,6 +10,12 @@ class Solution(object):
     To save more time, use the short list (S) as iterator, the long
     list (L) as hash checking.
       >>> filter(lambda k: k in L, S)
+
+    In some scenes, there are many duplicated values in the lists, so
+    it should unique them first to reduce the amount of values, which
+    saves more time for intersection.
+      >>> reduce(unique, nums1, {})
+      >>> reduce(unique, nums2, {})
     """
     def intersection(self, nums1, nums2):
         """
@@ -21,16 +27,19 @@ class Solution(object):
             if k not in dct:
                 dct[k] = 1
             return dct
+        nums1 = reduce(unique, nums1, {})
+        nums2 = reduce(unique, nums2, {})
         if len(nums1) < len(nums2):
             S, L = nums1, nums2
         else:
             S, L = nums2, nums1
-        res1 = filter(lambda k: k in L, S)
-        res2 = reduce(unique, res1, {})
-        return res2.keys()
+        res = filter(lambda k: k in L, S)
+        return res
 
 if __name__ == "__main__":
-    nums1 = [1, 2, 2, 3]
-    nums2 = [2, 2]
+    # nums1 = [1, 2, 2, 3]
+    # nums2 = [2, 2]
+    nums1 = ('1 2 2'*100000).split(' ')
+    nums2 = ('1 2 1'*10000).split(' ')
     s = Solution()
     print(s.intersection(nums1, nums2))
