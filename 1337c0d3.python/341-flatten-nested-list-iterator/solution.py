0525602dd62b61768119a15afb9ae7178d8d3e69
Chen Gui <gui.g.chen@gmail.com>
2016-05-21 17:38:46 +0800

    Submit 'Flatten Nested List Iterator' solution
    44 / 44 test cases passed.
    Status: Accepted
    Runtime: 132 ms
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/341-flatten-nested-list-iterator/solution.py
+++ 1337c0d3.python/341-flatten-nested-list-iterator/solution.py
@@ -3,26 +3,38 @@ from __future__ import print_function
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
-#class NestedInteger(object):
-#    def isInteger(self):
-#        """
-#        @return True if this NestedInteger holds a single integer, rather than a nested list.
-#        :rtype bool
-#        """
-#
-#    def getInteger(self):
-#        """
-#        @return the single integer that this NestedInteger holds, if it holds a single integer
-#        Return None if this NestedInteger holds a nested list
-#        :rtype int
-#        """
-#
-#    def getList(self):
-#        """
-#        @return the nested list that this NestedInteger holds, if it holds a nested list
-#        Return None if this NestedInteger holds a single integer
-#        :rtype List[NestedInteger]
-#        """
+class NestedInteger(object):
+    def __init__(self, val):
+        """
+        Initailize NestedInteger
+        """
+        if isinstance(val, list):
+            self.__value = map(NestedInteger, val)
+        else:
+            self.__value = val
+
+    def isInteger(self):
+        """
+        @return True if this NestedInteger holds a single integer, rather than a nested list.
+        :rtype bool
+        """
+        return isinstance(self.__value, int)
+
+    def getInteger(self):
+        """
+        @return the single integer that this NestedInteger holds, if it holds a single integer
+        Return None if this NestedInteger holds a nested list
+        :rtype int
+        """
+        return self.__value
+
+    def getList(self):
+        """
+        @return the nested list that this NestedInteger holds, if it holds a nested list
+        Return None if this NestedInteger holds a single integer
+        :rtype List[NestedInteger]
+        """
+        return self.__value
 
 class NestedIterator(object):
 
@@ -38,12 +50,13 @@ class NestedIterator(object):
     def flat(self, nestedList):
         """
         :type nestedList: List[NestedInteger]
+        :rtype: List[Integer]
         """
         for i in nestedList:
-            if isinstance(i, list):
-                self.__flat = self.flat(i)
+            if i.isInteger():
+                self.__flat.append(i.getInteger())
             else:
-                self.__flat.append(i)
+                self.__flat = self.flat(i.getList())
         return self.__flat
 
     def next(self):
@@ -66,6 +79,7 @@ class NestedIterator(object):
 if __name__ == "__main__":
     lst = [[1, 1], 2, [1, 1]]
     # lst = [1, [4,[6]]]
+    lst = [NestedInteger([1,1]), NestedInteger(2), NestedInteger([1,1])]
     i, v = NestedIterator(lst), []
     while i.hasNext(): v.append(i.next())
     print(v)
