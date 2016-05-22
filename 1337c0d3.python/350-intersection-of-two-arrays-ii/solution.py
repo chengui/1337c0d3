31c6f2b9ed2ab0b1f26446bc2ef7a3b39e53e103
Chen Gui <gui.g.chen@gmail.com>
2016-05-22 10:04:47 +0800

    Update count and extend by using single lambda function
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/350-intersection-of-two-arrays-ii/solution.py
+++ 1337c0d3.python/350-intersection-of-two-arrays-ii/solution.py
@@ -1,34 +1,31 @@
 from __future__ import print_function
 
 class Solution(object):
+    """
+    First count the numbers in both nums1 and nums2, and then check out the values
+    in both nums1 and nums2 with the less counts to generate a duplicated list
+      >>> reduce(lambda l, t: t[0] not in L and l or l.extend([t[0]]*min(L[t[0]], t[1])) or l, S.items(), [])
+    """
     def intersect(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
         """
-        def count(dct, k):
-            if k not in dct:
-                dct[k] = 1
-            else:
-                dct[k] += 1
-            return dct
+        count = lambda d, k: d.update({k: k in d and d[k]+1 or 1}) or d
         nums1 = reduce(count, nums1, {})
         nums2 = reduce(count, nums2, {})
         if len(nums1) < len(nums2):
             S, L = nums1, nums2
         else:
             S, L = nums2, nums1
-        def extend(lst, k):
-            if k[0] in L:
-                lst.extend([k[0]]*min(L[k[0]], k[1]))
-            return lst
+        extend = lambda l, t: t[0] not in L and l or l.extend([t[0]]*min(L[t[0]], t[1])) or l
         return reduce(extend, S.items(), []) 
 
 if __name__ == "__main__":
-    # nums1 = [1, 2, 2, 1]
-    # nums2 = [2, 2]
-    nums1 = [1, 2]
-    nums2 = [1, 1]
+    nums1 = [1, 2, 2, 1]
+    nums2 = [2, 2]
+    # nums1 = [1, 2]
+    # nums2 = [1, 1]
     s = Solution()
     print(s.intersect(nums1, nums2))
