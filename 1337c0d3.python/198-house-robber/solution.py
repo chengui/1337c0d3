28b9af44eac95cebae57d175316447126f47fab8
Chen Gui <gui.g.chen@gmail.com>
2016-05-26 21:41:26 +0800

    Submit 'House Robber' solution
    69 / 69 test cases passed.
    Status: Accepted
    Runtime: 52 ms
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/198-house-robber/solution.py
+++ 1337c0d3.python/198-house-robber/solution.py
@@ -7,16 +7,16 @@ class Solution(object):
     this part to nums[2:]; another is first room not selected,
     reduce this part to nums[1:]
       >>> max(nums[0], rob(nums[2:]), rob(nums[1:]))
+    Reduce solution. Iterate the recursive solution by storing each
+    previous previous maxinum sum and previous maxinum sum
+      >>> reduce(lambda t, x: (t[1], max(t[0]+x, t[1])), nums, (0, 0))[1]
     """
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 0: return 0
-        if len(nums) == 1: return nums[0]
-        if len(nums) == 2: return max(nums[0], nums[1])
-        return max(nums[0] + self.rob(nums[2:]), self.rob(nums[1:]))
+        return reduce(lambda t, x: (t[1], max(t[0]+x, t[1])), nums, (0, 0))[1]
 
 if __name__ == "__main__":
     nums = [3, 1, 1, 6, 8] 
