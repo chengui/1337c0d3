35650bc282be286e738cbf4e1ec4cc59f6b0463a
Chen Gui <gui.g.chen@gmail.com>
2016-05-26 12:23:51 +0800

    Submit 'Increasing Triplet Subsequence'
    61 / 61 test cases passed.
    Status: Accepted
    Runtime: 64 ms
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/334-increasing-triplet-subsequence/solution.py
+++ 1337c0d3.python/334-increasing-triplet-subsequence/solution.py
@@ -1,32 +1,33 @@
 from __future__ import print_function
 
 class Solution(object):
+    """
+    Use a ladder to assist, it tracks max values in max-len-is-one
+    sequence, max-len-is-two sequence, max-len-is-three sequence,
+    so if produced max-len-is-three sequence, return True; else
+    update the max values in these sequences
+    """
     def increasingTriplet(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        if len(nums) < 2: return False
-        return self.increasingTripletRecursive(nums[:], nums[0], 3)
-
-    def increasingTripletRecursive(self, nums, minNum, maxLen):
-        if maxLen <= 0:
-            return True
-        if len(nums) == 0:
-            return False
-        if len(nums) == 1:
-            if nums[0] > minNum and maxLen == 1: return True
-            return False
-        if nums[0] < nums[1]:
-            a = self.increasingTripletRecursive(nums[2:], nums[1], maxLen-2)
-            nums[0], nums[1] = nums[1], nums[0]
-            b = self.increasingTripletRecursive(nums[1:], nums[1], maxLen)
-            return a or b
-        return self.increasingTripletRecursive(nums[1:], nums[1], maxLen)
+        if len(nums) < 3: return False
+        ladder = [nums[0], None, None]
+        for i in range(1, len(nums)):
+            if ladder[2] is not None: return True
+            if ladder[1] is not None and nums[i] > ladder[1]:
+                ladder[2] = nums[i]
+            elif nums[i] > ladder[0]:
+                ladder[1] = nums[i]
+            else:
+                ladder[0] = nums[i]
+        return ladder[2] is not None
 
 if __name__ == "__main__":
     # nums = [1, 2, 3, 4, 5]
     # nums = [5, 4, 3, 2, 1]
-    nums = [1, 3, 2, 3, 2, 4]
+    # nums = [1, 3, 2, 3, 2, 4]
+    nums = range(10000, 0, -1)
     s = Solution()
     print(s.increasingTriplet(nums))
