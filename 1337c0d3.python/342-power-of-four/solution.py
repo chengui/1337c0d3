6d9a6a4c9bbcac82ce55e6b957480e2310124fbc
Chen Gui <gui.g.chen@gmail.com>
2016-06-01 23:13:20 +0800

    Submit 'Power of Four' solution
    1060 / 1060 test cases passed.
    Status: Accepted
    Runtime: 64 ms
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/342-power-of-four/solution.py
+++ 1337c0d3.python/342-power-of-four/solution.py
@@ -9,23 +9,21 @@ class Solution(object):
     Pattern of Power-of-Four is the amount of 1 in 32 bits
     always keeping 1, and this 1 only staying at even bit.
     Like: 0001b, 0100b
+
+    Use reduce to compute 1 bits in num:
+      >>> reduce(lambda s, x: s+((num>>x)&1), xrange(0,32,2), 0)
     """
     def isPowerOfFour(self, num):
         """
         :type num: int
         :rtype: bool
         """
-        cnt = 0
-        for i in xrange(0, 32):
-            if cnt > 1:
-                return False
-            if i % 2 == 1 and num & 1 == 1:
-                return False
-            cnt += num & 1
-            num = num >> 1
-        return cnt == 1
+        if num == 1: return True
+        if num % 2 == 1: return False
+        if reduce(lambda s, x: s + ((num>>x)&1), xrange(1, 32, 2), 0) > 0: return False
+        return reduce(lambda s, x: s + ((num>>x)&1), xrange(0, 32, 2), 0) == 1
 
 if __name__ == "__main__":
-    num = 16
+    num = 14
     s = Solution()
     print(s.isPowerOfFour(num))
