8f90f7dccd437a189dcaa3e583919bf0b5bc151b
Chen Gui <gui.g.chen@gmail.com>
2016-06-01 22:58:19 +0800

    Submit 'Power of Four' solution
    1060 / 1060 test cases passed.
    Status: Accepted
    Runtime: 60 ms
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/342-power-of-four/solution.py
+++ 1337c0d3.python/342-power-of-four/solution.py
@@ -5,13 +5,25 @@ class Solution(object):
     Assigned a signed 32bits integer, so it will be in such
     a map:
       >>> map(lambda x: 1<<x, range(0, 32, 2))
+
+    Pattern of Power-of-Four is the amount of 1 in 32 bits
+    always keeping 1, and this 1 only staying at even bit.
+    Like: 0001b, 0100b
     """
     def isPowerOfFour(self, num):
         """
         :type num: int
         :rtype: bool
         """
-        return num in map(lambda x: 1<<x, range(0, 32, 2))
+        cnt = 0
+        for i in xrange(0, 32):
+            if cnt > 1:
+                return False
+            if i % 2 == 1 and num & 1 == 1:
+                return False
+            cnt += num & 1
+            num = num >> 1
+        return cnt == 1
 
 if __name__ == "__main__":
     num = 16
