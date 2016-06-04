14be675c3efc97d14c51a12ac0efb32e77a11930
Chen Gui <gui.g.chen@gmail.com>
2016-06-04 11:34:36 +0800

    Submit 'House Robber III' solution
    124 / 124 test cases passed.
    Status: Accepted
    Runtime: 80 ms
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/337-house-robber-iii/solution.py
+++ 1337c0d3.python/337-house-robber-iii/solution.py
@@ -6,29 +6,45 @@ class TreeNode(object):
         self.val = x
         self.left = None
         self.right = None
+    def __str__(self):
+        return "%s" % self.val
 
 class Solution(object):
+    """
+    Recursive solution. Compute 'left' as left selected, 'lleft' as left unselected,
+    and 'right' as right selected, 'rright' as right unselected, thus the final result
+    will be max(left+right/*root unselected*/, root.val+lleft+rright/*root selected*/)
+
+    Post-order traversal. Calculate two values (in, out), which means w/ or w/o root
+    selected. if w/ root, get rid of left and right children (supposed to be lout, rout),
+    which results out root.val+lout+rout; else four cases would be considered, as like:
+    (w/-left+w/right, w/o-left+w/right, w/-left+w/o-right, w/o-left+w/o-right), check out
+    the maximum value from them. The final result will be the maximum of the two.
+    """
     def rob(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if root is None:
-            return 0
-        lleft, rright, left, right = 0, 0, 0, 0
-        if root.left is not None:
-            left = self.rob(root.left)
-            lleft = self.rob(root.left.left) + self.rob(root.left.right)
-        if root.right is not None:
-            rigth = self.rob(root.right)
-            rright = self.rob(root.right.left) + self.rob(root.right.right)
-        return max(left+right, root.val+lleft+rright)        
+        def traversal(root):
+            if root is None: return (0, 0)
+            (lin, lout) = traversal(root.left)
+            (rin, rout) = traversal(root.right)
+            return (root.val+lout+rout, max(lin+rin, lout+rin, lin+rout, lout+rout))
+        if root is None: return 0
+        return max(traversal(root))
 
 if __name__ == "__main__":
+    # root = TreeNode(3)
+    # root.left = TreeNode(2)
+    # root.right = TreeNode(3)
+    # root.left.right = TreeNode(3)
+    # root.right.right = TreeNode(1)
     root = TreeNode(3)
-    root.left = TreeNode(2)
-    root.right = TreeNode(3)
-    root.left.right = TreeNode(3)
+    root.left, root.right = TreeNode(4), TreeNode(5)
+    root.left.left, root.left.right = TreeNode(3), TreeNode(1)
     root.right.right = TreeNode(1)
+    # root = TreeNode(4)
+    # root.left, root.left.left, root.left.left.left = TreeNode(1), TreeNode(2), TreeNode(3)
     s = Solution()
     print(s.rob(root))
