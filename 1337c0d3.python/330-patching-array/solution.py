783710d3afb5f6e008fbd97f9ea8c9223562dadd
Chen Gui <gui.g.chen@gmail.com>
2016-06-18 19:27:36 +0800

    Submit 'Patching Array' solution
    Status: Runtime Error
    Runtime Error Message:
    Line 19: ValueError: math domain error
    Last executed input:
    [2,4,14,18,20,25,25,35,73,94]
    42
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/330-patching-array/solution.py
+++ 1337c0d3.python/330-patching-array/solution.py
@@ -12,24 +12,26 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        patches = []
-        bpow, mxrange = 0, 0
-        nums.append(n)
-        while mxrange < n:
-            if 2**bpow not in nums:
-                patches.append(2**bpow)
-            else:
-                nums.remove(2**bpow)
-            bpow, mxrange = bpow + 1, 2**(1+bpow) - 1
-        mxrange += num
-        if mxrange >= n: break
-        return len(patches)
+        import math
+        import itertools
+        nlen, plen, patches, removes = 1, 0, dict(), dict()
+        nums = filter(lambda i: i<=n, nums)
+        while nlen <= len(nums):
+            for com in itertools.combinations(range(len(nums)), nlen):
+                nsum = sum(map(nums.__getitem__, com))
+                if nsum & (nsum - 1) == 0 and nsum not in patches:
+                    patches[nsum] = True
+                    map(lambda i: nums.__setitem__(i, -1), com)
+            nums = filter(lambda i: i != -1, nums)
+            nlen += 1
+        return 1 + int(math.log(n - sum(nums), 2)) - len(patches)
        
 if __name__ == "__main__":
     # nums, n = [1, 3], 6
     # nums, n = [1, 2, 2], 5
     # nums, n = [1, 5, 10], 20
     # nums, n = [], 8
-    nums, n = [1, 2, 31, 33], 2147483647
+    # nums, n = [1, 2, 31, 33], 2147483647
+    nums, n = [2, 4, 14, 18, 20, 25, 25, 35, 73, 94], 42
     s = Solution()
     print(s.minPatches(nums, n))
