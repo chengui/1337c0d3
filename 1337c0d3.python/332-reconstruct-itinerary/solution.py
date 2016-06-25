0cec3d2130f4e63cada8fa51bfe28767c0aa54d1
Chen Gui <gui.g.chen@gmail.com>
2016-06-25 10:44:39 +0800

    Submit 'Reconstruct Itinerary' solution
    20 / 79 test cases passed.
    Status: Wrong Answer
    Input:
    [["EZE","TIA"],["EZE","HBA"],["AXA","TIA"],["JFK","AXA"],["ANU","JFK"],["ADL","ANU"],["TIA","AUA"],["ANU","AUA"],["ADL","EZE"],["ADL","EZE"],["EZE","ADL"],["AXA","EZE"],["AUA","AXA"],["JFK","AXA"],["AXA","AUA"],["AUA","ADL"],["ANU","EZE"],["TIA","ADL"],["EZE","ANU"],["AUA","ANU"]]
    Output:
    ["JFK","AXA","AUA","ADL","ANU","AUA","ANU","EZE","ADL","EZE","ANU","JFK","AXA","EZE","HBA","TIA","ADL","EZE","AUA","AXA","TIA"]
    Expected:
    ["JFK","AXA","AUA","ADL","ANU","AUA","ANU","EZE","ADL","EZE","ANU","JFK","AXA","EZE","TIA","AUA","AXA","TIA","ADL","EZE","HBA"]
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/332-reconstruct-itinerary/solution.py
+++ 1337c0d3.python/332-reconstruct-itinerary/solution.py
@@ -9,23 +9,34 @@ class Solution(object):
         import bisect
         def dfs_visit(G, u, visited, vpath):
             if u not in G:
+                print(">>> ", visited, vpath)
                 return
+            visited[u] = True
+            vpath.append(u)
             for v in G[u]:
-                if (u, v) not in visited:
-                    visited[(u, v)] = True
-                    vpath.append((v))
+                if v not in visited:
                     dfs_visit(G, v, visited, vpath)
-        graph, visit, vpath = dict(), dict(), list()
-        for t in tickets:
-            if t[0] not in graph: graph[t[0]] = []
-            bisect.insort(graph[t[0]], t[1])
-        dfs_visit(graph, 'JFK', visit, vpath)
-        vpath.insert(0, 'JFK')
-        return vpath
+            return vpath
+        tomap, graph = dict(), dict()
+        for n, t in enumerate(tickets):
+            if t[0] not in tomap: tomap[t[0]] = []
+            bisect.insort(tomap[t[0]], (t[1], n))
+        for n, t in enumerate(tickets):
+            if t[1] not in tomap:
+                graph[n] = []
+            else:
+                graph[n] = map(lambda x: x[1], tomap[t[1]])
+        for to, i in tomap['JFK']:
+            visit, vpath = dict(), list()
+            dfs_visit(graph, i, visit, vpath)
+            if len(vpath) == len(tickets):
+               return ['JFK'] + map(lambda x: tickets[x][1], vpath)
+        return ['JFK'] + map(lambda x: tickets[x][1], vpath)
 
 if __name__ == "__main__":
     # tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
-    tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+    # tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
     # tickets = [["JFK", "ATL"], ["JFK", "SFO"], ["SFO", "MUC"], ["MUC", "JFK"]]
+    tickets = [["EZE","TIA"],["EZE","HBA"],["AXA","TIA"],["JFK","AXA"],["ANU","JFK"],["ADL","ANU"],["TIA","AUA"],["ANU","AUA"],["ADL","EZE"],["ADL","EZE"],["EZE","ADL"],["AXA","EZE"],["AUA","AXA"],["JFK","AXA"],["AXA","AUA"],["AUA","ADL"],["ANU","EZE"],["TIA","ADL"],["EZE","ANU"],["AUA","ANU"]]
     s = Solution()
     print(s.findItinerary(tickets))
