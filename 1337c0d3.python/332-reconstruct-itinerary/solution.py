cd48927bb21e4c37b51c33a10489cb5572cf6d8c
Chen Gui <gui.g.chen@gmail.com>
2016-06-25 10:42:02 +0800

    Submit 'Reconstruct Itinerary' solution
    9 / 79 test cases passed.
    Status: Wrong Answer
    Input:
    [["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]
    Output:
    ["JFK","KUL","NRT","JFK"]
    Expected:
    ["JFK","NRT","JFK","KUL"]
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 1337c0d3.python/332-reconstruct-itinerary/solution.py
+++ 1337c0d3.python/332-reconstruct-itinerary/solution.py
@@ -6,22 +6,26 @@ class Solution(object):
         :type tickets: List[List[str]]
         :rtype: List[str]
         """
+        import bisect
         def dfs_visit(G, u, visited, vpath):
             if u not in G:
-                G[u] = ['JFK']
-            visited[u] = True
-            vpath.append(u)
+                return
             for v in G[u]:
-                if v not in visited:
+                if (u, v) not in visited:
+                    visited[(u, v)] = True
+                    vpath.append((v))
                     dfs_visit(G, v, visited, vpath)
-        vpath = list()
-        visited = dict()
-        graph = reduce(lambda d, l: d.update({l[0]: d.get(l[0], []) + [l[1]]}) or d, tickets, {})
-        dfs_visit(graph, 'JFK', visited, vpath)
+        graph, visit, vpath = dict(), dict(), list()
+        for t in tickets:
+            if t[0] not in graph: graph[t[0]] = []
+            bisect.insort(graph[t[0]], t[1])
+        dfs_visit(graph, 'JFK', visit, vpath)
+        vpath.insert(0, 'JFK')
         return vpath
 
 if __name__ == "__main__":
     # tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
     tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+    # tickets = [["JFK", "ATL"], ["JFK", "SFO"], ["SFO", "MUC"], ["MUC", "JFK"]]
     s = Solution()
     print(s.findItinerary(tickets))
