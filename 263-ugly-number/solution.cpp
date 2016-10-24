efa1dc0891ffd28a61fed79997b452e6eb1e5824
Gui Chen <gui.g.chen@gmail.com>
2016-10-25 00:51:42 +0800

    Submit 'Ugly Number' (cpp)
    1012 / 1012 test cases passed.
    Status: Accepted
    Runtime: 3 ms
    
    Signed-off-by: Gui Chen <gui.g.chen@gmail.com>

---
+++ 263-ugly-number/solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0) return false;
+        if (num == 1) return true;
+        while (num > 1)
+        {
+            if (num % 2 == 0)
+                num = num / 2;
+            else if (num % 3 == 0)
+                num = num / 3;
+            else if (num % 5 == 0)
+                num = num / 5;
+            else
+                return false;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    int num = 14;
+    Solution s = Solution();
+    cout << s.isUgly(num) << endl;
+}
