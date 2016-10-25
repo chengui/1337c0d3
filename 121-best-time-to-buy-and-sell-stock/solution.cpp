9d6894cbf72521374cb2d7aa071ab4dd73deedf6
Chen Gui <gui.g.chen@gmail.com>
2016-10-25 10:17:05 +0800

    Submit 'Best Time to Buy and Sell Stock' (cpp)
    200 / 200 test cases passed.
    Status: Accepted
    Runtime: 6 ms
    
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- 121-best-time-to-buy-and-sell-stock/solution.cpp
+++ 121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -5,28 +5,24 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        if (prices.empty())
+    int maxProfit(vector<int> &prices) {
+        if (prices.size() == 0)
             return 0;
-        int n = prices.size();
-        int max = 0, min = 0;
-        int profit = 0, maxprofit = 0;
-        max = min = prices[n - 1];
-        for (int i = n - 1; i >= 0; i--)
+        int profit = 0;
+        int save_profit = 0;
+        int buyprice = prices[0];
+        for (size_t i = 0; i < prices.size(); i++)
         {
-            if (prices[i] > max)
+            if (prices[i] <= buyprice)
+                buyprice = prices[i];
+            else
             {
-                max = min = prices[i];
-            }
-            else if (prices[i] < min)
-            {
-                min = prices[i];
-                profit = max - min;
-                if (profit > maxprofit)
-                    maxprofit = profit;
+                profit = prices[i] - buyprice;
+                if (profit > save_profit)
+                    save_profit = profit;
             }
         }
-        return maxprofit;
+        return save_profit;
     }
 };
 
