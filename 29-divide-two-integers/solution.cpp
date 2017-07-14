4a57f4d9a8c08e0cfeeb0d0a38fc805e2e81df74
Gui Chen <gui.g.chen@gmail.com>
2017-07-14 19:53:39 +0800

    Submit 'Divide Two Integers'
    Status: Accepted
    Runtime: 60 ms
    
    Signed-off-by: Gui Chen <gui.g.chen@gmail.com>

--- 29-divide-two-integers/solution.cpp
+++ 29-divide-two-integers/solution.cpp
@@ -3,10 +3,38 @@
 using namespace std;
 
 class Solution {
-    public:
+public:
     int divide(int dividend, int divisor) {
-            return dividend/divisor;
+        if (dividend == divisor)
+            return 1;
+
+        int mask = 0x80000000;
+        int sub = 0, res = 0, bits = 32;
+        int flag_dividend = dividend & mask;
+        int flag_divisor = divisor & mask;
+        if (flag_dividend)
+            dividend = ~dividend + 1;
+        if (flag_divisor)
+            divisor = ~divisor + 1;
+
+        while (bits) {
+            if (dividend & mask)
+                sub = (sub << 1) | 1;
+            else
+                sub = sub << 1;
+            if ((sub - divisor) & mask) {
+                res = res << 1;
+            } else {
+                sub = sub - divisor;
+                res = (res << 1) | 1;
+            }
+            dividend <<= 1;
+            bits--;
         }
+        if (flag_dividend ^ flag_divisor)
+            return (~res + 1);
+        return res;
+    }
 };
 
 int main() {
